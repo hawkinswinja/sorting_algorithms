33aafa3cc317d46de0c2404bfe751d0f54ffdc38
hawkinswinja <hawkinswinja@gmail.com>
2022-10-15 22:50:01 +0300

    betty test for 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -22,9 +22,14 @@ void selection_sort(int *array, size_t size)
 		}
 		n++;
 	}
-		
-
 }
+/**
+  * minimum - returns the smallest element in array
+  * @array: array of integers
+  * @n: size of the array
+  * @index: start of array to check
+  * Return: position of the smallest element
+  */
 int minimum(int *array, size_t n, size_t index)
 {
 	size_t m = index;
